786394d0005c345e5b7d56ee783067adaa46ef07
Mauricio Erazo <antonierazo13@hotmail.com>
2025-08-13 23:14:02 +0200

    refactor: update Layout component structure with header and main sections

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,8 +3,17 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <>
-      <div>Layout</div>
-      <Outlet />
+      <header className="bg-gray-200">
+        <div className="mx-auto max-w-6xl py-10">
+          <div className="text-4xl font-semibold text-sky-700">
+            Product Administration
+          </div>
+        </div>
+      </header>
+
+      <main className='mt-10 mx-auto max-w-6xl p-10 bg-white rounded-md shadow'>
+        <Outlet />
+      </main>
     </>
   );
 };
