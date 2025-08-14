1ca62e170bc5f01c4b5f23e0a212d16e1cec09f3
Mauricio Erazo <antonierazo13@hotmail.com>
2025-08-15 01:56:56 +0200

    refactor: enhance NewProduct and Products components with structured layout and navigation links

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const NewProduct = () => {
-  return <div>NewProduct</div>;
+  return (
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl text-slate-800 font-semibold">New Product</h2>
+        <Link
+          to="/"
+          className="rounded-md bg-cyan-600 p-3 text-white font-semibold shadow hover:bg-cyan-700 transition duration-200"
+        >
+          Back to Products
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default NewProduct;
--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,8 +1,19 @@
+import { Link } from 'react-router-dom';
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl text-slate-800 font-semibold">Products</h2>
+        <Link
+          to="newProduct"
+          className="rounded-md bg-cyan-600 p-3 text-white font-semibold shadow hover:bg-cyan-700 transition duration-200"
+        >
+          Add Product
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default Products
+export default Products;
