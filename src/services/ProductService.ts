7dce0f58db67995c971ef099306bb74d6bb8609c
Mauricio Erazo <antonierazo13@hotmail.com>
2025-08-16 15:50:22 +0200

    feat: sent post request to RestAPI

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,7 +1,27 @@
+import axios from 'axios';
+import { safeParse } from 'valibot';
+import { DraftProductSchema } from '../types';
+
 type ProductData = {
   [k: string]: FormDataEntryValue;
 };
 
 export const addProduct = async (data: ProductData) => {
-  console.log(data);
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price,
+    });
+    if (result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products`;
+      const { data } = await axios.post(url, {
+        name: result.output.name,
+        price: result.output.price,
+      });
+    } else {
+      throw new Error('Not valid Data');
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
