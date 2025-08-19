c890ef57bc77d31aafc949589fc3557931646719
Mauricio Erazo <antonierazo13@hotmail.com>
2025-08-19 19:00:49 +0200

    feat: enhance product availability display with styled badge

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -13,8 +13,16 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
       <td className="p-3 text-lg text-gray-800">
         {formatCurrency(product.price)}
       </td>
-      <td className="p-3 text-lg text-gray-800">
-        {isAvailable ? 'Available' : 'Not Available'}
+      <td className="p-3 text-lg">
+        <span
+          className={`inline-flex items-center rounded-md px-2 py-1 text-lg font-medium ${
+            isAvailable
+              ? 'bg-green-50 text-green-700 inset-ring inset-ring-green-600/20'
+              : 'bg-pink-50 text-pink-700 inset-ring inset-ring-pink-700/10'
+          }`}
+        >
+          {isAvailable ? 'Available' : 'Not Available'}
+        </span>
       </td>
       <td className="p-3 text-lg text-gray-800"></td>
     </tr>
