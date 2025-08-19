f6230037929d2ebe671d28e3bbb6eb5d7e146691
Mauricio Erazo <antonierazo13@hotmail.com>
2025-08-19 22:21:52 +0200

    fix: update font weight for product name and price in ProductDetails component

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -14,8 +14,8 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
 
   return (
     <tr className="border-b">
-      <td className="p-3 text-lg text-gray-800">{product.name}</td>
-      <td className="p-3 text-lg text-gray-800">
+      <td className="p-3 text-lg font-medium text-gray-800">{product.name}</td>
+      <td className="p-3 text-lg font-medium text-gray-800">
         {formatCurrency(product.price)}
       </td>
       <td className="p-3 text-lg">
